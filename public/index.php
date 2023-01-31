514a7a23d96750d8559707328959250e627564d4
Ivan Popov <popovbm@gmail.com>
2023-01-31 16:05:40 +0300

    1

--- public/index.php
+++ public/index.php
@@ -4,10 +4,11 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
-use Appp\Validator;
+use App\Validator;
 
-$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+const USERS = 'users.txt';
 
 session_start();
 
@@ -22,6 +23,7 @@ $container->set('flash', function () {
 
 AppFactory::setContainer($container);
 $app = AppFactory::create();
+$app->add(MethodOverrideMiddleware::class);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -32,15 +34,13 @@ $app->get('/', function ($request, $response) {
 $app->get('/users', function ($request, $response) {
     $term = $request->getQueryParam('term');
     $messages = $this->get('flash')->getMessages();
-    $users = json_decode(file_get_contents('user.txt'), true);
-    if (isset($term)) {
-        $filteredUsers = array_filter($users, function ($user) use ($term) {
-            return str_contains($user['userId'], $term);
-        });
-        $params = ['users' => $filteredUsers, 'term' => $term, 'flash' => $messages];
-    } else {
-        $params = ['users' => $users, 'flash' => $messages];
-    }
+    $users = json_decode(file_get_contents(USERS), true);
+    $result = collect($users)->filter(fn ($user) => empty($term) ? true : str_contains(strtolower($user['name']), strtolower($term)));
+    $params = [
+        'users' => $result,
+        'flash' => $messages,
+        'term' => $term
+    ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('users');
 
@@ -54,25 +54,29 @@ $app->get('/users/new', function ($request, $response) {
 
 $app->get('/users/{id}', function ($request, $response, $args) {
     $id = $args['id'];
-    $users = json_decode(file_get_contents('user.txt'), true);
+    $users = json_decode(file_get_contents(USERS), true);
+    $user = collect($users)->firstWhere('id', $id);
 
-    $isFinded = false;
-    $findedUser = [];
-    foreach ($users as $user) {
-        if ($user['userId'] === $id) {
-            $isFinded = true;
-            $findedUser = $user;
-            break;
-        }
-    }
-    if ($isFinded === false) {
-        return $response->write('Page not found')
-            ->withStatus(404);
+    if (is_null($user)) {
+        return $response->write('Page not found')->withStatus(404);
     }
 
-    $params = ['id' => $findedUser['userId'], 'nickname' => $findedUser['nickname'], 'email' => $findedUser['email']];
+    $params = ['id' => $user['id'], 'name' => $user['name'], 'email' => $user['email']];
     return $this->get('renderer')->render($response, '/users/show.phtml', $params);
 })->setName('user');
+
+$app->get('/users/{id}/edit', function ($request, $response, array $args) {
+    $messages = $this->get('flash')->getMessages();
+    $id = $args['id'];
+    $users = json_decode(file_get_contents(USERS), true);
+    $user = collect($users)->firstWhere('id', $id);
+    $params = [
+        'user' => $user,
+        'flash' => $messages,
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+})->setName('editUser');
 
 $router = $app->getRouteCollector()->getRouteParser();
 
@@ -83,11 +87,10 @@ $app->post('/users', function ($request, $response) use ($router) {
     $validator = new Validator();
     $errors = $validator->validate($userData);
     if (count($errors) === 0) {
-        $data = ['userId' => $id, 'nickname' => $userData['user']['name'], 'email' => $userData['user']['email']];
-        $file = 'user.txt';
-        $current = json_decode(file_get_contents($file));
+        $data = ['id' => $id, 'name' => $userData['name'], 'email' => $userData['email']];
+        $current = json_decode(file_get_contents(USERS));
         $current[] = $data;
-        file_put_contents($file, json_encode($current));
+        file_put_contents(USERS, json_encode($current));
 
         $this->get('flash')->addMessage('success', 'User has been created');
         $url = $router->urlFor('users');
@@ -99,7 +102,41 @@ $app->post('/users', function ($request, $response) use ($router) {
         'errors' => $errors
     ];
     $response = $response->withStatus(422);
-    return $response->get('renderer')->render($response, '/users/new.phtml', $params);
+    return $this->get('renderer')->render($response, '/users/new.phtml', $params);
+});
+
+$app->patch('/users/{id}', function ($request, $response, array $args) use ($router) {
+    $id = $args['id'];
+    $users = json_decode(file_get_contents(USERS), true);
+    $user = collect($users)->firstWhere('id', $id);
+    $data = $request->getParsedBodyParam('user');
+
+    $validator = new Validator();
+    $errors = $validator->validate($data);
+
+    if (count($errors) === 0) {
+        $updatedUsers = collect($users)->each(function ($user) use ($id, $data) {
+            if ($user['id'] === $id) {
+                $item['name'] = $data['name'];
+            }
+        })->toArray();
+        // foreach ($users as $user) {
+        //     if ($user['id'] === $id) {
+        //         $user['name'] = $data['name'];
+        //         break;
+        //     }
+        // }
+        file_put_contents(USERS, json_encode($updatedUsers));
+        $this->get('flash')->addMessage('success', 'User has been updated');
+        $url = $router->urlFor('editUser', ['id' => $user['id']]);
+        return $response->withRedirect($url);
+    }
+    $params = [
+        'user' => $user,
+        'errors' => $errors
+    ];
+    $response = $response->withStatus(422);
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
 });
 
 $app->run();
--- src/Validator.php
+++ src/Validator.php
@@ -1,18 +1,21 @@
 <?php
 
-namespace Appp;
+namespace App;
 
 class Validator
 {
     public function validate($user)
     {
         $errors = [];
-        if (count($user['name']) < 4) {
+        if (strlen($user['name']) < 4) {
             $errors['name'] = 'Nickname must be greather than 4 characters';
         }
-        if (count($user['email']) < 4) {
-            $errors['email'] = 'Email must be greather then 4 characters';
+        if (isset($user['email'])) {
+            if (strlen($user['email']) < 4) {
+                $errors['email'] = 'Email must be greather then 4 characters';
+            }
         }
+
         return $errors;
     }
 }
